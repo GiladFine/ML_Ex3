1ff467bd27bc8aff633c5beef50708b8cb79c1fc
Gilad Fine <gilad.fine1@gmail.com>
2019-06-14 13:26:41 +0300

    - Hyper params separated from the network class - Documentation

--- ex3.py
+++ ex3.py
@@ -2,7 +2,13 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 NUM_OF_CLASSES = 10
+
+# Neural Network hyper params
+LEARNING_RATE = 0.05
 TRAIN_VALIDATION_RATIO = 0.85
+NUM_OF_EPOCHS = 35
+MINI_BATCH_SIZE = 32
+HIDDEN_LAYER_SIZE = 100
 
 
 def show_accuracy_graph(accuracies, title):
@@ -10,7 +16,6 @@ def show_accuracy_graph(accuracies, title):
     plt.ylabel("Accuracy")
     plt.xlabel("Epoch Number")
     plt.title(title)
-    plt.show()
 
 
 def write_predictions(output_path, test_predictions):
@@ -31,23 +36,31 @@ def reLU_gradient(x):
 
 
 class NeuralNetwork(object):
-    def __init__(self, num_of_classes, training_and_validation_set, learning_rate=0.05,
-                 hidden_layer_size=100, num_of_epochs=20):
-        self.full_batch_size = 32
+    """
+    Neural Network with 1 hidden layer
+    """
+    def __init__(self, num_of_classes, train_set):
         self.num_of_classes = num_of_classes
-        self.num_of_epochs = num_of_epochs
-        self.learning_rate = learning_rate
-        self.w_input = np.random.uniform(-0.035, 0.035, (hidden_layer_size, training_and_validation_set[0][0].shape[0]))
-        self.b_input = np.array([[0.0]] * hidden_layer_size)
-        self.w_hidden = np.random.uniform(-0.1415, 0.1415, (self.num_of_classes, hidden_layer_size))
+        self.train_set = train_set
+
+        # Weights of input layer & bias
+        self.w_input = np.random.uniform(-0.035, 0.035, (HIDDEN_LAYER_SIZE, self.train_set[0][0].shape[0]))
+        self.b_input = np.array([[0.0]] * HIDDEN_LAYER_SIZE)
+
+        # Weights of hidden layer & bias
+        self.w_hidden = np.random.uniform(-0.1415, 0.1415, (self.num_of_classes, HIDDEN_LAYER_SIZE))
         self.b_hidden = np.array([[0.0]] * self.num_of_classes)
-        self.params = None
 
     def predict(self, record):
         values = self.forward_propagation(record)
         return np.argmax(values["hidden"]["outputs"])
 
     def forward_propagation(self, record):
+        """
+        Stream the data record in the network, saving each layer's input & output for back-prop
+        :param record: data record
+        :return: dictionary containing original record, and each layer's input & outputs values vectors
+        """
         input_layer_inputs = np.dot(self.w_input, record) + self.b_input
         input_layer_outputs = reLU(input_layer_inputs)
         hidden_layer_inputs = np.dot(self.w_hidden, input_layer_outputs) + self.b_hidden
@@ -57,6 +70,11 @@ class NeuralNetwork(object):
                 "hidden": {"inputs": hidden_layer_inputs, "outputs": hidden_layer_outputs}}
 
     def back_propagation(self, records_values):
+        """
+        Modify the network's weights using the Back-Prop method
+        :param records_values: list of forward_propagation's dictionaries outputs, the size of the list is MINI_BATCH_SIZE at most
+        :return: dictionary containing the gradients vectors calculated for each layer
+        """
         # Initialize the gradients
         w_input_gradient = np.zeros(self.w_input.shape)
         b_input_gradient = np.zeros(self.b_input.shape)
@@ -89,30 +107,37 @@ class NeuralNetwork(object):
                 "hidden": {"weights": w_hidden_gradient, "bias": b_hidden_gradient}}
 
     def update_weights(self, gradients):
-        self.b_input -= self.learning_rate * gradients["input"]["bias"]
-        self.w_input -= self.learning_rate * gradients["input"]["weights"]
-        self.b_hidden -= self.learning_rate * gradients["hidden"]["bias"]
-        self.w_hidden -= self.learning_rate * gradients["hidden"]["weights"]
+        """
+        Update weights vectors using the gradients
+        :param gradients: the dictionary returned by back-propagation, containing gradients vectors for each layer
+        """
+        self.b_input -= LEARNING_RATE * gradients["input"]["bias"]
+        self.w_input -= LEARNING_RATE * gradients["input"]["weights"]
+        self.b_hidden -= LEARNING_RATE * gradients["hidden"]["bias"]
+        self.w_hidden -= LEARNING_RATE * gradients["hidden"]["weights"]
 
     def train(self):
+        """
+        Trains the Neural Network
+        """
 
         # Split data to train & validation sets
-        train_set = training_and_validation_set[:int(TRAIN_VALIDATION_RATIO * len(training_and_validation_set))]
-        validation_set = training_and_validation_set[int(TRAIN_VALIDATION_RATIO * len(training_and_validation_set)):]
+        training_set = self.train_set[:int(TRAIN_VALIDATION_RATIO * len(self.train_set))]
+        validation_set = self.train_set[int(TRAIN_VALIDATION_RATIO * len(self.train_set)):]
 
+        # Init accuracies arrays
         training_accuracies = []
         validation_accuracies = []
 
         # Foreach epoch
-        for epoch_number in range(self.num_of_epochs):
-            print(epoch_number)
-            np.random.shuffle(train_set)
+        for epoch_number in range(NUM_OF_EPOCHS):
+            np.random.shuffle(training_set)
 
             train_correct_predictions_counter = 0
             batch = []
 
             # Foreach record on train_set
-            for record in train_set:
+            for record in training_set:
                 # Propagate & save the records values at each step, including the correct class
                 record_values = self.forward_propagation(record[0])
                 record_values["correct class"] = record[1]
@@ -123,7 +148,7 @@ class NeuralNetwork(object):
                     train_correct_predictions_counter += 1
 
                 # If the batch size reached, run back propagation
-                if self.full_batch_size == len(batch):
+                if len(batch) == MINI_BATCH_SIZE:
                     self.update_weights(self.back_propagation(batch))
                     batch = []
 
@@ -138,31 +163,30 @@ class NeuralNetwork(object):
                     validation_correct_predictions_counter += 1
 
             # Calc & append accuracies
-            training_accuracies.append(float(train_correct_predictions_counter) / len(train_set))
+            training_accuracies.append(float(train_correct_predictions_counter) / len(training_set))
             validation_accuracies.append(float(validation_correct_predictions_counter) / len(validation_set))
 
-            print("Epoch number %d, batch size = %d, training accuracy = %f, validation accuracy = %f" % \
-                  (epoch_number,
-                   self.full_batch_size,
-                   training_accuracies[-1],
-                   validation_accuracies[-1]
-                   ))
+            print("Epoch number %d, training accuracy = %f, validation accuracy = %f" % (epoch_number,
+                                                                                         training_accuracies[-1],
+                                                                                         validation_accuracies[-1]
+                                                                                         ))
 
         # Show accuracy graphs
         show_accuracy_graph(training_accuracies, "Training Accuracy Graph")
         show_accuracy_graph(validation_accuracies, "Validation Accuracy Graph")
+        plt.savefig("Accuracies.png")
 
 
 if __name__ == "__main__":
     # Normalize, shuffle & add classification to train_x
-    training_and_validation_set = list(zip(
+    train_set = list(zip(
         np.expand_dims(np.loadtxt("train_x", dtype=np.uint8), axis=2) / 255.0,
         np.loadtxt("train_y", dtype=np.uint8)
     ))
-    np.random.shuffle(training_and_validation_set)
+    np.random.shuffle(train_set)
 
     # Create & train the neural network
-    neural_network = NeuralNetwork(NUM_OF_CLASSES, training_and_validation_set)
+    neural_network = NeuralNetwork(NUM_OF_CLASSES, train_set)
     neural_network.train()
 
     # Run on test set
@@ -171,4 +195,5 @@ if __name__ == "__main__":
     for record in test_set:
         predictions.append(neural_network.predict(record))
 
+    # Print test_y
     write_predictions("test_y", predictions)
